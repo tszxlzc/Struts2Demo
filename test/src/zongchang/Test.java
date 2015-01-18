d2ff463b38f28cc33b1463f1082057f23e290373
liuzongchang <tszxlzc@126.com>
2015-01-18 09:18:44 +0800

    my first project committed to git

---
+++ test/src/zongchang/Test.java
@@ -0,0 +1,13 @@
+package zongchang;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.print("hello");
+	}
+
+}
